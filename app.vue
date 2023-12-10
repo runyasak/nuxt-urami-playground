5b8768455f9a89c777b0a3142fd3c002357753e7
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-12-10 12:48:50 +0700

    initiate

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <UramiImage
+      src="https://demo.rayriffy.com/tom-scott.jpg"
+      width="801"
+      height="801"
+      alt="Tom Scott"
+    />
+  </div>
+</template>
